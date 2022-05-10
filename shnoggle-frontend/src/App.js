c7ce107d3d591a0ffd02405ff899367c55bf1248
CrackerJaps <thecrackerjaps@gmail.com>
2022-05-10 00:01:21 -0700

    import front end template - add backend packages

---
+++ shnoggle-frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./styles.scss"
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+      <p>text text text text</p>
+    </div>
+  )
+}
---
+++ shnoggle-frontend/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/",
+  },
+  devServer: {
+    client: {
+      logging: "none",
+    },
+    open: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss?$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)?$/,
+        use: "file-loader?name=./img/[name].[ext]",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+    }),
+  ],
+  stats: {
+    children: true,
+  },
+}
