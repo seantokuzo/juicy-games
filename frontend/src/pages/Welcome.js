62ccdc76674a0f2aba1826879cf49a8515a7e880
CrackerJaps <thecrackerjaps@gmail.com>
2022-06-20 23:54:03 -0700

    remove gsap

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,27 +1,15 @@
 import React, { useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useAppContext } from '../context/appContext'
-import { gsap } from 'gsap'
 
 export default function Welcome() {
   const { categories, retrieveCategories, updateGameOptions } = useAppContext()
-  const welcomeRef = useRef()
-  const q = gsap.utils.selector(welcomeRef)
   console.log(categories)
-  
-  useEffect(() => {
-    gsap.from(q('.welcome__title'), { x: -100 })
-  }, [])
 
   useEffect(() => {
     fetch('https://opentdb.com/api_category.php')
       .then((res) => res.json())
       .then((data) => {
-        // setGameOptions((prev) => ({
-        //   ...prev,
-        //   category: data.trivia_categories[0].id.toString()
-        // }))
-        updateGameOptions('category', data.trivia_categories[0].id.toString())
         retrieveCategories(
           data.trivia_categories.slice(0, data.trivia_categories.length - 2)
         )
@@ -60,7 +48,7 @@ export default function Welcome() {
   // )
 
   return (
-    <section className="welcome page" ref={welcomeRef}>
+    <section className="welcome page">
       <h1 className="welcome__title page__title">Trivial Trivia</h1>
       <div className="welcome__links-div">
         {/* {loginForm} */}
