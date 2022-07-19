8c0ecb1a307f6a248f9fae8fd3b7a661069fe82d
seantokuzo <seantokuzo@gmail.com>
2022-07-18 19:46:01 -0700

    add user model pre save and custom instance methods

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import validator from 'validator'
-// import bcrypt from 'bcryptjs'
-// import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -28,5 +28,24 @@ const UserSchema = new mongoose.Schema({
     select: false
   }
 })
+
+UserSchema.pre('save', async function () {
+  // CHECK IF PASSWORD IS MODIFIED
+  if (!this.isModified('password')) return
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME
+  })
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
+}
 
 export default mongoose.model('User', UserSchema)
