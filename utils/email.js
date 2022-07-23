e8f4ecbb79a313fc2a12cdbe8ad84a02ee874231
seantokuzo <seantokuzo@gmail.com>
2022-07-22 23:02:31 -0700

    import pug and nodemailer for emails

---
+++ utils/email.js
@@ -0,0 +1,78 @@
+import nodemailer from 'nodemailer'
+import pug from 'pug'
+import { convert } from 'html-to-text'
+
+export class Email {
+  constructor(user, url) {
+    this.to = user.email
+    this.username = user.username
+    this.url = url
+    this.from = `Trivial Trivia <${process.env.EMAIL_FROM}>`
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // SendGrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      })
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    })
+  }
+  // Send the actual email
+  async send(template, subject) {
+    // 1) RENDER HTML BASED ON A PUG TEMPLATE
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      username: this.username,
+      url: this.url,
+      subject
+    })
+    // 2) DEFINE EMAIL OPTIONS
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html, {
+        wordwrap: 130
+      })
+    }
+
+    // 3) CREATE A TRANSPORT AND SEND EMAIL
+
+    await this.newTransport().sendMail(mailOptions)
+  }
+
+  async sendEmailConfirm() {
+    await this.send(
+      'confirmEmail',
+      'Trivial Trivia | Please confirm your email address'
+    )
+  }
+
+  async sendWelcome() {
+    await this.send(
+      'welcome',
+      'Trivial Trivia | Welcome to your worst nightmare!'
+    )
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordResetEmail',
+      'Trivial Trivia | Password Reset (You got 10 minutes, tick tock...)'
+    )
+  }
+}
