ab950f51db7633513a28ff88a69de1e7cec2ad70
seantokuzo <seantokuzo@gmail.com>
2022-08-03 11:41:56 -0700

    error page style

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div className="error page">
-      <h1 className="title">Page Not Found</h1>
-      <h3 className="subtitle">how'd you get over here?</h3>
-      <Link to="/" className="btn form-btn">
-        <h3>Back</h3>
-      </Link>
-    </div>
+    <section className="page">
+      <div className="error">
+        <h1 className="error__title title">Page Not Found</h1>
+        <h3 className="error__subtitle subtitle">How'd you get over here?</h3>
+        <Link to="/" className="btn btn-theme form-btn error__btn">
+          <h3>Back</h3>
+        </Link>
+      </div>
+    </section>
   )
 }
 
