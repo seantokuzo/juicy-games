4ff2ca226dea193e5f2d7ad9ff170eb8f82e4238
seantokuzo <seantokuzo@gmail.com>
2022-08-24 13:42:28 -0700

    reorder game menu btns

--- client/src/components/games/GamesMenu.js
+++ client/src/components/games/GamesMenu.js
@@ -7,14 +7,14 @@ const GamesMenu = () => {
     <div className="form games__menu">
       <h1 className="games__menu-title title">Games</h1>
       <ButtonLink
-        path="/me/games/trivial-trivia"
-        btnClass="btn-theme link account__links-link fs-trivia"
-        text="Trivial Trivia"
-      />
-      <ButtonLink
         path="/me/games/boredle"
         btnClass="btn-theme link account__links-link fs-boredle"
         text="Boredle"
+      />
+      <ButtonLink
+        path="/me/games/trivial-trivia"
+        btnClass="btn-theme link account__links-link fs-trivia"
+        text="Trivial Trivia"
       />
     </div>
   )
