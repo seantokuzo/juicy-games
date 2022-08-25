11296cdc922bb88daeda50abf86544a5bba13a50
seantokuzo <seantokuzo@gmail.com>
2022-08-24 19:39:27 -0700

    new game btn if game over

--- client/src/components/games/boredle/game/components/modals/AlertModal.js
+++ client/src/components/games/boredle/game/components/modals/AlertModal.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import NewGameBtn from '../NewGameBtn'
 import { useBoredleContext } from '../../../../../../context/boredle-context/boredleContext'
 
-const AlertModal = ({ text }) => {
+const AlertModal = ({ text, practiceOver }) => {
   const { alertText } = useBoredleContext()
 
   return (
     <div className={`boredle__alert theme-color theme-border`}>
       <h3>{alertText || text}</h3>
+      {practiceOver && <NewGameBtn />}
     </div>
   )
 }
