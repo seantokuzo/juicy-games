e8b5ebe2f566fc6381159310006e3100e3f3867b
seantokuzo <seantokuzo@gmail.com>
2022-08-24 19:40:52 -0700

    update views

--- client/src/components/games/boredle/BoredlePractice.js
+++ client/src/components/games/boredle/BoredlePractice.js
@@ -1,21 +1,36 @@
 import React, { useEffect } from 'react'
 import BoredleGame from './game/BoredleGame'
-import { AlertModal } from './game/components'
+import { AlertModal, NewGameBtn } from './game/components'
 import { useBoredleContext } from '../../../context/boredle-context/boredleContext'
 import { decryptBoredle } from '../../../utils/boredleEncrypt'
 
 const BoredlePractice = () => {
-  const { practice, getPracticeWord } = useBoredleContext()
-  const { didLose, answer } = practice
+  const { practice, getPracticeWord, getLocalPractice, newPracticeGame } =
+    useBoredleContext()
+  const { didWin, didLose, answer } = practice
 
   useEffect(() => {
+    const localPractice = localStorage.getItem('practice')
+    console.log(localPractice)
+    if (localPractice) return getLocalPractice()
     getPracticeWord()
   }, [])
 
+  const newGameBtn = (
+    <div className="boredle__practice-new-game boredle__alert theme-color theme-border">
+      <NewGameBtn />
+    </div>
+  )
+
   return (
     <div className="boredle__practice page">
-      {didLose && (
-        <AlertModal text={`Answer: ${decryptBoredle(answer).join('')}`} />
+      {(didWin || didLose) && (
+        <>
+          <AlertModal
+            text={`Answer: ${decryptBoredle(answer).join('')}`}
+            practiceOver={true}
+          />
+        </>
       )}
       <BoredleGame />
     </div>
