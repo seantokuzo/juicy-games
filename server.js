dd5403f2223007dc54c43a10d504ac21603b772d
seantokuzo <seantokuzo@gmail.com>
2023-01-26 18:08:42 -0800

    update cors options

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config()
 import http from 'http'
 import { Server } from 'socket.io'
 
-// import cors from 'cors'
+import cors from 'cors'
 import 'express-async-errors'
 import morgan from 'morgan'
 
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV !== 'production') {
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.use(express.static(path.resolve(__dirname, './client/build')))
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 // SECURITY PACKAGES
@@ -67,9 +67,9 @@ const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
     // DEVELOPMENT
-    origin: 'http://localhost:8080',
+    // origin: 'http://localhost:8080',
     // PRODUCTION
-    // origin: 'https://juicy-games.onrender.com/',
+    origin: 'https://juicy-games.onrender.com/',
     credentials: true
   }
 })
