7b6cc6a1186fe56228ff3dabcfa8948457c6e8b4
seantokuzo <seantokuzo@gmail.com>
2023-04-19 11:58:16 -0700

    change base urls to production

--- client/src/utils/baseURLs.js
+++ client/src/utils/baseURLs.js
@@ -1,7 +1,7 @@
 // PRODUCTION
-// export const axiosBaseURL = '/api/v1/'
-// export const socketBaseURL = '/'
+export const axiosBaseURL = '/api/v1/'
+export const socketBaseURL = '/'
 
 // DEVELOPMENT
-export const axiosBaseURL = 'http://localhost:5000/api/v1/'
-export const socketBaseURL = 'http://localhost:5000'
+// export const axiosBaseURL = 'http://localhost:5000/api/v1/'
+// export const socketBaseURL = 'http://localhost:5000'
